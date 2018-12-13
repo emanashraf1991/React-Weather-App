f557cec34a492eed37eadd6c1d808b224d1afb47
Eman Ashraf <emanashraf1991@gmail.com>
2018-12-12 20:28:57 -0800

    import bootstrap pkg to start designing weather app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.min.js";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
